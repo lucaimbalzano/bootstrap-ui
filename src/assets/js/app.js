96ee1d2f20bc27e25e1f60a10f4066cc7d3ed408
luca <lucaimbalzano@gmail.com>
2021-08-08 22:41:16 +0200

    ##Burger menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,6 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navbar-links');
+    const nav = document.querySelector('.navbar-links li');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('navbar-active');
