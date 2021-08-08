86403ea79def72b054a9dcdd2d0ce614540a5667
luca <lucaimbalzano@gmail.com>
2021-08-08 22:32:57 +0200

    ##making burger menu

---
+++ src/assets/js/app.js
@@ -0,0 +1,9 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navbar-active');
+    });
+}
+navSlide();
