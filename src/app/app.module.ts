7dd211a925063707979b72baf279da9d03c46154
luca <lucaimbalzano@gmail.com>
2021-08-08 17:12:19 +0200

    ##Making the UI Style form scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocsComponent } from './components/docs/docs/docs.component';
+import { ExamplesComponent } from './components/examples/examples/examples.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocsComponent,
+    ExamplesComponent
   ],
   imports: [
     BrowserModule,
